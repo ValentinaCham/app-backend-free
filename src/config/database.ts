116a181ec2774ec963813d123da2fb77e8e000c8
Valentina Chambilla <vchambillap@unsa.edu.oe>
2025-03-14 15:07:04 -0500

    update: env variables

--- src/config/database.ts
+++ src/config/database.ts
@@ -4,9 +4,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME as string,
-  process.env.DB_USER as string,
-  process.env.DB_PASS,
+  process.env.MYSQLDB_DATABASE as string,
+  process.env.MYSQLDB_USER as string,
+  process.env.MYSQLDB_ROOT_PASSWORD,
   {
     host: process.env.DB_HOST as string,
     port: Number(process.env.DB_PORT),
